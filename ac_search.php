2f93c69f4bfe62853c590e90f9dae5c8048adf73
Vishesh Yadav <codeignition@gmail.com>
2010-06-23 22:31:03 +0530

    Search Info Pop Up Box in Autocompletion.
    A pop up dialog box is displayed when an option
    is selected from the result pop up menu. At the
    moment the dialog shows TAG NAME, URL and HITS
    COUNTER.
    
    To obtain data related to the TAG for the pop
    up, AJAX request is made to search-info.php
    file. This file is temporary and will be
    integrated with other search services in
    single file.

--- ac_search.php
+++ ac_search.php
@@ -12,7 +12,18 @@
         source: "search.php",
         minLength: 2,
         select: function(event, ui) {
-          //
+          $("#tag-name").text(ui.item.value);
+          $.ajax({
+            type: "GET",
+            url: "search-info.php",
+            data: "term="+ui.item.value,
+            dataType: "json",
+            success: function(data) {
+              $("#tag-url").html('<a href="' + data[0] + '">' + data[0] + "</a>");
+              $("#tag-hits").text(data[1]);
+            }
+          });  
+          $("#search-info").dialog('open');   
         }
       })
         .data("autocomplete")._renderItem = function(ul, item) {
@@ -24,6 +35,15 @@
                           "<u><b>$1</b></u>") + "</a>")  .   appendTo(ul);
         };
 
+        $("#search-info").dialog({
+          modal: true,
+          autoOpen: false,
+          draggable: false,
+          minWidth: 600,
+          resizable: false,
+          width: 600 
+        });
+
     });
   </script>
   </head>
@@ -32,6 +52,13 @@
     <div class="ui-widgets">
       <label for="search">Search: </label>
       <input id="search" />
+    </div>
+    <div id="search-info" title="Search Info">      
+      <p>
+        <h1 align="center" id="tag-name">TAG NAME</h1>
+        <h5 align="center" id="tag-url">URL</h5>
+        <h1 align="center" id="tag-hits">HITS</h1>
+      </p>
     </div>
   </body>
 
